2f119759fa0f6c0be2794ed7007833aaec102dac
thomas dumont <thomas2dumont@gmail.com>
2022-12-04 20:00:57 +0100

    tiny adjustment

--- src/TP_template/shaders/text3DAlpha.fs.glsl
+++ src/TP_template/shaders/text3DAlpha.fs.glsl
@@ -12,10 +12,15 @@ uniform vec3 uLightPos;
 out vec4 fFragColor;
 
 vec3 tint=vec3(109.f/255.f,48.f/255.f,194.f/255.f);
-float tintPow=.3f;
+
+vec2 uv=vPosition_vs.xy/vec2(1920,1080);
+float tintPow=.5f;
+
+vec3 c1=vec3(.4039,.349,.8235);
+vec3 c2=vec3(.8235,.349,.349);
 
 void main(){
     float light=min(1.f/pow(distance(uLightPos,vPosition_vs),2.f),1.f);
     vec4 textColor=texture(uTexture,vTexCoords.xy);
-    fFragColor=vec4(light*mix(textColor.xyz,tint,tintPow),textColor.w);
+    fFragColor=vec4(light*mix(textColor.xyz,mix(c1,c2,textColor.y),tintPow),textColor.w);
 }
